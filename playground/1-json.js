7ffb83bf84f608255457e295a67b8d8c8238d955
Michael <print@3daddict.com>
2019-04-29 19:20:30 -0700

    wrtieFileSync Challenge Completed

--- playground/1-json.js
+++ playground/1-json.js
@@ -18,8 +18,9 @@ const fs = require('fs');
 
 const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-data.name = "mike";
-data.age = 36
+const user = JSON.parse(dataJSON);
+user.name = "mike";
+user.age = 36
 
-console.log(data);
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('1-json.json', userJSON);
